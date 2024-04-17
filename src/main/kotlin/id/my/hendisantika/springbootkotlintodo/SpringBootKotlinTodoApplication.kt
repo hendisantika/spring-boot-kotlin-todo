3bc457745c7f3bf935689f67cb64bd4ec43b3082
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 06:59:00 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/springbootkotlintodo/SpringBootKotlinTodoApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.springbootkotlintodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinTodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinTodoApplication>(*args)
+}
