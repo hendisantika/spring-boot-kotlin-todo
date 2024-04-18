dab4021ce8e42db21baec5487e6c790acf0ee1dd
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:12:06 +0700

    Add rethrowConsumer in LambdaExceptionUtil 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootkotlintodo/util/LambdaExceptionUtil.java
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/util/LambdaExceptionUtil.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.springbootkotlintodo.util;
 
+import java.util.function.Consumer;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-kotlin-todo
@@ -19,5 +21,18 @@ public final class LambdaExceptionUtil {
     @FunctionalInterface
     public interface Function_WithExceptions<T, R, E extends Exception> {
         R apply(T t) throws E;
+    }
+
+    /**
+     * .forEach(rethrowConsumer(name -> System.out.println(Class.forName(name))));
+     */
+    public static <T, E extends Exception> Consumer<T> rethrowConsumer(Consumer_WithExceptions<T, E> consumer) throws E {
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception exception) {
+                throwActualException(exception);
+            }
+        };
     }
 }
