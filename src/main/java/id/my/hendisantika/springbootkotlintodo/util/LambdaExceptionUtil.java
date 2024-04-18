8581f8f5aba1767aa26383669b1458dbc78c6d55
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:11:30 +0700

    Add LambdaExceptionUtil 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/util/LambdaExceptionUtil.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.springbootkotlintodo.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-todo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/18/24
+ * Time: 07:11
+ * To change this template use File | Settings | File Templates.
+ */
+public final class LambdaExceptionUtil {
+}
