4065486907c20ddfcf63b77f882c70940c71ec55
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:08:19 +0700

    Add findAllBy in TodoRepository 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootkotlintodo/repository/TodoRepository.java
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/repository/TodoRepository.java
@@ -2,10 +2,13 @@ package id.my.hendisantika.springbootkotlintodo.repository;
 
 import id.my.hendisantika.springbootkotlintodo.model.Todo;
 import jakarta.persistence.QueryHint;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Stream;
 
@@ -26,5 +29,12 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "" + Integer.MIN_VALUE))
     @Query(value = "select t from Todo t")
     Stream<Todo> streamAll();
+
+    /**
+     * We need this method variant instead of one that returns Page because methods that return Page
+     * always execute count query, which is slow when using pagination in batch to export large tables.
+     */
+    @Transactional(readOnly = true)
+    Slice<Todo> findAllBy(Pageable page);
 
 }
