e5ba9efd842d38d7c56ed3f24097d9037f964822
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:05:31 +0700

    Change Kotlin to Java 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/SpringBootKotlinTodoApplication.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootkotlintodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-todo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/18/24
+ * Time: 07:04
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class SpringBootKotlinTodoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKotlinTodoApplication.class);
+    }
+}
