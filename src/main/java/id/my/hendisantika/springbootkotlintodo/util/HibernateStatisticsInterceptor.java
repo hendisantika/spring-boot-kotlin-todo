38276ba078f6bb2da8890a3ad6c53811ef19a66a
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:08:53 +0700

    Add HibernateStatisticsInterceptor 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/util/HibernateStatisticsInterceptor.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootkotlintodo.util;
+
+import org.hibernate.internal.EmptyInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-todo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/18/24
+ * Time: 07:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class HibernateStatisticsInterceptor extends EmptyInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateStatisticsInterceptor.class);
+
+    private final ThreadLocal<Long> queryCount = new ThreadLocal<>();
+}
