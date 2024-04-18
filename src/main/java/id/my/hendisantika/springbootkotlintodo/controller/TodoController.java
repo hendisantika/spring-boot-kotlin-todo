f6d77973773a64c02661980bf38f9b5823a94cec
hendisantika <hendisantika@yahoo.co.id>
2024-04-18 07:18:12 +0700

    Add exportTodosCSV in TodoController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootkotlintodo/controller/TodoController.java
+++ src/main/java/id/my/hendisantika/springbootkotlintodo/controller/TodoController.java
@@ -4,16 +4,23 @@ import id.my.hendisantika.springbootkotlintodo.model.Todo;
 import id.my.hendisantika.springbootkotlintodo.repository.TodoRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Stream;
+
+import static id.my.hendisantika.springbootkotlintodo.util.LambdaExceptionUtil.rethrowConsumer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -53,10 +60,30 @@ public class TodoController {
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/todos", method = RequestMethod.POST)
+    @PostMapping(value = "/todos")
     public String createTodo(@Valid TodoDto todoDto) {
         todoRepository.save(todoDto.toTodo());
         return "redirect:/";
     }
 
+    @GetMapping(value = "/todos.csv")
+    @Transactional(readOnly = true)
+    public void exportTodosCSV(HttpServletResponse response) {
+        response.addHeader("Content-Type", "application/csv");
+        response.addHeader("Content-Disposition", "attachment; filename=todos.csv");
+        response.setCharacterEncoding("UTF-8");
+        try (Stream<Todo> todoStream = todoRepository.streamAll()) {
+            PrintWriter out = response.getWriter();
+            todoStream.forEach(rethrowConsumer(todo -> {
+                String line = todoToCSV(todo);
+                out.write(line);
+                out.write("\n");
+                entityManager.detach(todo);
+            }));
+            out.flush();
+        } catch (IOException e) {
+            log.info("Exception occurred " + e.getMessage(), e);
+            throw new RuntimeException("Exception occurred while exporting results", e);
+        }
+    }
 }
